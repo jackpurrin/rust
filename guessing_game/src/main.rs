46b7ba98c3838cc28bb3795f0d7e9763d193221a
𝖏𝖆𝖈𝖐𝖕𝖚𝖗𝖗𝖎𝖓.𝖒𝖊 <me@jackpurrin.me>
2025-05-12 15:00:19 -0700

    okay adds some rng to it

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -10,6 +10,11 @@ fn main() { // main function of the program, all the code goes into here to get 
     // sets a variable, guess. normally, variables are `immutable` which means it cannot change. 
     // but if you specify `mut`, it will be mutable, i can change it after inputting it. 
     // the `String::New()` just creates a new empty string that we can change later.
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    // defines a new variable, secret_number. it's equal to a random number between 1-100.
+    // rand just says use the rand library
+    // thread_rng just means that is local to the current thread of execution and is seeded by the operating system
+    // gen_range is just the range that the number will be generate from.
     io::stdin() // from the io module, we'll use stdin. stdin allows for user input
         .read_line(&mut guess) 
         // .read_line handles user input, from the keyboard.
@@ -21,4 +26,5 @@ fn main() { // main function of the program, all the code goes into here to get 
     // this is the last line of code. this outputs "You guessed: {guess}"
     // but how? the {} indicates that the second param, guess, should be placed here.
     // resulting in "You guessed: {number you guessed}"
+    prinntln!("You guessed: {secret_number}") // reapplying the principles from last line to this line
 }
